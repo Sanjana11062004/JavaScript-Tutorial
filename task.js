a1039d11f2dee8784647402c2acf6f438695e130
Sanjana M <sanjanamurugesan06@gmail.com>
2023-06-15 01:03:31 +0530

    .

--- task.js
+++ task.js
@@ -9,4 +9,23 @@ switch(res+" sanju"){
     default:
         alert("Wrong input");
 }
+//for loop
+for(i=1;i<=10;i++){
+    document.write("<h2>"+num+"*"+i+"="+(num*i)+"</h2>");
+}
 alert("The sum of 19 and 22 is "+res)
+//while loop
+document.write("<br />")
+var x=1;
+while(x<=10){
+    document.write(x);
+    x+=2;
+}
+document.write("<br />")
+//function
+function add(a,b){
+    var total=a+b;
+    return total;
+}
+var c= add("sanjana",1);
+document.write("<h1>"+c+"</h1>");
