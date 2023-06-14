451db5f5bb7fc0720580d696e25cc7db111b93fe
Sanjana M <sanjanamurugesan06@gmail.com>
2023-06-15 00:14:01 +0530

    .

--- task.js
+++ task.js
@@ -1,1 +1,12 @@
-document.write("hii")
+var num=19;
+var num2=22;
+var res;
+res=num+num2;
+switch(res+" sanju"){
+    case "41 sanju":
+        document.write("Correct Answer");
+        break;
+    default:
+        alert("Wrong input");
+}
+alert("The sum of 19 and 22 is "+res)
